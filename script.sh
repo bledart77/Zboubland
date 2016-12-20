9dc7dfb4aefe4b69ed391d451d18e2763a5aa538
Mathieu COLAS DES FRANCS <mcolas-d@e3r7p12.42.fr>
2016-12-20 09:21:25 +0100

    r

--- script.sh
+++ script.sh
@@ -1,9 +1,16 @@
-rm ./libft/*
-rm ./fillit/*
-rm ./get_next_line/*
+rm -rf libft
+rm -rf fillit
+rm -rf get_next_line
+mkdir libft
+mkdir fillit
+mkdir get_next_line
+mkdir get_next_line/libft
+mkdir get_next_line/libft/include
 cp ../Projects/libft/* libft/
 cp ../Projects/fillit/* fillit/
-cp ../Projects/get_next_line//* get_next_line/
+cp ../Projects/get_next_line/* get_next_line/
+cp ../Projects/get_next_line/libft/libft.a get_next_line/libft/
+cp ../Projects/get_next_line/libft/include/libft.h get_next_line/libft/include
 git add ./*
 git commit -m 'r'
 git push
